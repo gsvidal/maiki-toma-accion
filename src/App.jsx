8a1ca05bc77785f3533d5578ac172becd4ee0f22
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-08-08 11:56:48 -0500

    Add and setup a React Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
-import { Subscription } from "./components/Subscription/Subscription";
-import { Bio } from "./components/Bio/Bio";
-import { Contact } from "./components/Contact/Contact";
+import { AppRoutes } from "./AppRoutes";
 
 function App() {
   const [page, setPage] = useState("subscribe");
 
-  // let pageName = page == "subscribe" ?
-
   return (
     <>
-      <Header setPage={setPage} />
-      <main>
-        {page === "subscribe" ? (
-          <Subscription />
-        ) : page === "bio" ? (
-          <Bio />
-        ) : (
-          <Contact />
-        )}
-      </main>
-      <Footer></Footer>
+      <Header />
+      <AppRoutes />
+      <Footer />
     </>
   );
 }
---
+++ src/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { Subscription } from "./components/Subscription/Subscription";
+import { Bio } from "./components/Bio/Bio";
+import { Contact } from "./components/Contact/Contact";
+import { Route, Routes } from "react-router-dom";
+import { NotFound } from "./components/NotFound/NotFound";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/suscripcion" element={<Subscription />} />
+      <Route path="/maiki" element={<Bio />} />
+      <Route path="/contacto" element={<Contact />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  );
+};
